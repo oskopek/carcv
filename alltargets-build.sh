2bc415d8dd607ed283ce86338bf22a1798f5933f
Ondrej Skopek <skopekondrej@gmail.com>
2013-01-02 18:15:12 +0100

    Updated build script

--- alltargets-build.sh
+++ alltargets-build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 DASHES="------------------------------------------------------"
+basedir=$(pwd)
+
+
 echo $DASHES
 echo "Build script by odenkos"
 echo $DASHES
@@ -20,6 +23,7 @@ echo
 echo $DASHES
 echo "-------REMOVING BUILD DIR"
 echo $DASHES
+cd $basedir
 rm -rfv  build/
 
 echo
@@ -28,8 +32,19 @@ echo
 echo $DASHES
 echo "-------CREATING BUILD DIR"
 echo $DASHES
+cd $basedir
 mkdir build
-cd build
+
+echo
+echo
+echo
+echo $DASHES
+echo "-------COPYING SOURCE FILES OVER"
+echo $DASHES
+cd $basedir
+cd cardetect
+rm -rfv carmatcher* carrectangles*
+cp -v /home/odenkos/c_workspace/CarProject/cardetect/* .
 
 echo
 echo
@@ -37,6 +52,8 @@ echo
 echo $DASHES
 echo "-------RUNNING CMAKE"
 echo $DASHES
+cd $basedir
+cd build
 cmake ..
 
 echo
@@ -46,6 +63,14 @@ echo $DASHES
 echo "-------RUNNING MAKE"
 echo $DASHES
 make
+
+echo
+echo
+echo
+echo $DASHES
+echo "------DISTRIBUTING"
+echo $DASHES
+cp -v $basedir/build/cardetect/carrect $basedir/test/all/
 
 echo
 echo
