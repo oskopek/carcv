168d1b062a42e3802df8f50bdc17c9cce8fa520d
Ondrej Skopek <skopekondrej@gmail.com>
2013-08-22 00:10:21 +0200

    Updated CMakeLists for cpp_ocr_alpr, added some code to ocr_recognizer.cpp

---
+++ cpp_ocr_alpr/ocr_recognizer.cpp
@@ -0,0 +1,10 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include <opencv2/core/core.hpp>
+
+#include <boost/filesystem.hpp>
+
+using namespace cv;
+using namespace std;
+
+namespace fs = boost::filesystem;
