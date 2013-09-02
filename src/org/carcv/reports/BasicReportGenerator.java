13f74e7f973dafabc922ed5359306ad8a3a2eed7
Ondrej Skopek <skopekondrej@gmail.com>
2013-09-02 21:56:55 +0200

    Added Report generation stub
    Signed-off-by: Ondrej Skopek <skopekondrej@gmail.com>

---
+++ src/org/carcv/reports/BasicReportGenerator.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.carcv.reports;
+
+/**
+ * @author oskopek
+ *
+ */
+public class BasicReportGenerator {
+
+	/**
+	 * 
+	 */
+	public BasicReportGenerator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
