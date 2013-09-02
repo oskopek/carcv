1d38422374cfa99c9fce2bb0178c7798523f6a6b
Ondrej Skopek <skopekondrej@gmail.com>
2013-09-02 19:48:01 +0200

    Added SpeedUnit enum
    Signed-off-by: Ondrej Skopek <skopekondrej@gmail.com>

---
+++ src/org/carcv/model/SpeedUnit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.carcv.model;
+
+/**
+ * SpeedUnits:
+ * - MPH - miles per hour
+ * - KPH - kilometers per hour
+ * - MS - meters per second
+ * - KNOTS - nautical knots
+ * @author oskopek
+ *
+ */
+public enum SpeedUnit {
+	MPH, KPH, MS, KNOTS
+}
