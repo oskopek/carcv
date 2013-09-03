195f6b5c9aad0cc13a6be7f18e9ca71bf65f91e1
Ondrej Skopek <skopekondrej@gmail.com>
2013-09-03 23:20:39 +0200

    Added Report Generation and test
    Signed-off-by: Ondrej Skopek <skopekondrej@gmail.com>

--- src/org/carcv/reports/BasicReportGenerator.java
+++ src/org/carcv/reports/BasicReportGenerator.java
@@ -3,9 +3,27 @@
  */
 package org.carcv.reports;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRMapCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+
+import org.carcv.model.CarData;
 import org.carcv.model.Entry;
+import org.carcv.model.IAddress;
 
 
 /**
@@ -14,11 +32,67 @@ import org.carcv.model.Entry;
  */
 public class BasicReportGenerator {
 	
-	public static Object buildReport(Entry e) {
+	public static void buildPDFReport(Entry e, String templateFilename, String exportToFilename, String reportBuilderLocation, String reportName) {
+			
+		Map<String, Object> values = new HashMap<String, Object>();
+		CarData data = e.getData();
+		
+
+		DateFormat dateFormat = new SimpleDateFormat("dd. MM. yyyy"); 
+		DateFormat timeFormat = new SimpleDateFormat("HH:mm");
 		
 		
-		return null;
+		//report
+		values.put("reportid", Long.toString(System.currentTimeMillis()));
+		values.put("reportname", reportName);
+		values.put("reportlocation", reportBuilderLocation);
+		values.put("reportdate", dateFormat.format(new Date(System.currentTimeMillis())));
 		
+		//data
+		IAddress add = (IAddress) data.getLocation();
+		String dataLocation = add.street() + " " + add.streetNumber() + "/" + add.referenceNumber() + "\n"
+				+ add.postalcode() + " " + add.city() + "\n"
+				+ add.country();
+		
+		//values.put("id", Long.toString(data.getId()));
+		values.put("date", dateFormat.format(data.getTimestamp()));
+		values.put("location", dataLocation);
+		values.put("LPNumber", data.getLicencePlate().getText());
+		values.put("videoURL", data.getVideo().getURL());
+		values.put("time", timeFormat.format(data.getTimestamp()));
+		values.put("speed", Double.toString(data.getSpeed().getSpeed()) + " " + data.getSpeed().getUnit().toString());
+		
+		//values.put
+		
+		Collection<Map<String, ?>> mapList = new ArrayList<Map<String, ?>>();
+		mapList.add(values);
+		
+		JRMapCollectionDataSource mapDataSource = new JRMapCollectionDataSource(mapList);
+		
+		Map parameters = new HashMap();
+		
+		
+		try {
+			//compile template - already precompiled
+			//JasperCompileManager.compileReportToFile(templateFilename + ".jrxml", templateFilename + ".jasper");
+			
+			//fill with data
+			JasperPrint print = JasperFillManager.fillReport(templateFilename, parameters, mapDataSource);
+			
+			//export
+			JRExporter exporter = new JRPdfExporter();
+			
+			exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, exportToFilename);
+			exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
+			
+			exporter.exportReport();
+			
+		} catch (JRException jre) {
+			jre.printStackTrace();
+		}
+		
+		
+		return;
 	}
 
 }
---
+++ test/org/carcv/reports/BasicReportGeneratorTest.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package org.carcv.reports;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import net.sf.jasperreports.engine.JRException;
+
+import org.carcv.model.Address;
+import org.carcv.model.CarData;
+import org.carcv.model.Entry;
+import org.carcv.model.ILocation;
+import org.carcv.model.LicencePlate;
+import org.carcv.model.MediaObject;
+import org.carcv.model.MediaType;
+import org.carcv.model.Speed;
+import org.carcv.model.SpeedUnit;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author oskopek
+ *
+ */
+public class BasicReportGeneratorTest {
+	
+	private static Entry testEntry;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		MediaObject preview = new MediaObject("test.com/test.jpeg", MediaType.JPEG);
+		
+		Speed speed = new Speed(80d, SpeedUnit.KPH);
+		
+		Address add = new Address("Myjava", "90701", "Jablonská", "Slovakia", 27);
+		ILocation location = add;
+		
+		LicencePlate licencePlate = new LicencePlate("MY-077AU", "SK");
+		
+		Date timestamp = new Date(System.currentTimeMillis());
+		
+		MediaObject video = new MediaObject("test.com/video.h264", MediaType.H264);
+		
+		CarData carData = new CarData(speed, location, licencePlate, timestamp, video);
+		
+		testEntry = new Entry(carData, preview);
+	}
+
+	/**
+	 * Test method for {@link org.carcv.reports.BasicReportGenerator#buildPDFReport(org.carcv.model.Entry, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testBuildPDFReport() {
+		try {
+			BasicReportGenerator.buildPDFReport(testEntry, "./res/reports/speed_report.jasper", "./test_results/report" + System.currentTimeMillis() + ".pdf", "Myjava", "TestReport");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception in report build.");
+		}
+	}
+
+}
