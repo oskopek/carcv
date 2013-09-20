c80e15abf4e4f3678811de5c0479e5b613462ba3
Ondrej Skopek <skopekondrej@gmail.com>
2013-09-20 23:27:25 +0200

    remapped TestServlet, updated web.xml

--- src/main/java/org/carcv/servlet/TestServlet.java
+++ src/main/java/org/carcv/servlet/TestServlet.java
@@ -18,7 +18,7 @@ import org.carcv.model.MediaType;
 /**
  * Servlet implementation class TestServlet
  */
-@WebServlet("/servlet/TestServlet")
+@WebServlet("/test/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
