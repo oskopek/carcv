4e46c9828f8d30be9b42f1efa544f38c1f590ce2
Ondrej Skopek <skopekondrej@gmail.com>
2013-09-21 01:09:13 +0200

    Tiny fix in CarTableServlet

--- src/main/java/org/carcv/servlet/CarTableServlet.java
+++ src/main/java/org/carcv/servlet/CarTableServlet.java
@@ -80,7 +80,7 @@ public class CarTableServlet extends HttpServlet {
 			date = dateFormat.format(e.getData().getTimestamp());
 			time = timeFormat.format(e.getData().getTimestamp());
 			licencePlate = e.getData().getLicencePlate().getOrigin() + " - "
-					+ e.getData().getLicencePlate().getOrigin();
+					+ e.getData().getLicencePlate().getText();
 			location = e.getData().getLocation().toString();
 			videoURL = e.getData().getVideo().getURL();
 			previewURL = e.getPreview().getURL();
