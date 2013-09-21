c0b49e59236bc4718abe4ef89a443a96bd3363a3
Ondrej Skopek <skopekondrej@gmail.com>
2013-09-21 21:51:25 +0200

    Fixed resource handling in Reports

--- src/main/java/org/carcv/reports/BasicReportGenerator.java
+++ src/main/java/org/carcv/reports/BasicReportGenerator.java
@@ -3,6 +3,7 @@
  */
 package org.carcv.reports;
 
+import java.io.OutputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -84,6 +85,73 @@ public class BasicReportGenerator {
 		exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME,
 				exportToFilename);
 		exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
+
+		exporter.exportReport();
+
+		return;
+	}
+	
+	public void buildPDFReportStream(Entry e, String templateFilename,
+			String exportToFilename,
+			OutputStream out,
+			String reportBuilderLocation,
+			String reportName) throws JRException {
+		
+		Map<String, Object> values = new HashMap<String, Object>();
+		Map<String, Object> parameters = new HashMap<String, Object>();
+
+		CarData data = e.getData();
+
+		DateFormat dateFormat = new SimpleDateFormat("dd. MM. yyyy");
+		DateFormat timeFormat = new SimpleDateFormat("HH:mm");
+
+		// report
+		parameters.put("reportid", Long.toString(System.currentTimeMillis()));
+		parameters.put("reportname", reportName);
+		parameters.put("reportlocation", reportBuilderLocation);
+		parameters.put("reportdate",
+				dateFormat.format(new Date(System.currentTimeMillis())));
+
+		// data
+		IAddress add = data.getLocation();
+		String dataLocation = add.print();
+
+		// parameters.put("id", Long.toString(data.getId()));
+		parameters.put("previewURL", e.getPreview().getURL());
+		parameters.put("date", dateFormat.format(data.getTimestamp()));
+		parameters.put("location", dataLocation);
+		parameters.put("LPNumber", data.getLicencePlate().getText());
+		parameters.put("videoURL", data.getVideo().getURL());
+		parameters.put("time", timeFormat.format(data.getTimestamp()));
+		parameters.put("speed", Double.toString(data.getSpeed().getSpeed())
+				+ " " + data.getSpeed().getUnit().toString());
+
+		// parameters.put
+
+		Collection<Map<String, ?>> mapList = new ArrayList<Map<String, ?>>();
+		mapList.add(values);
+
+		JRMapCollectionDataSource mapDataSource = new JRMapCollectionDataSource(
+				mapList);
+
+		// compile template - already precompiled
+		// JasperCompileManager.compileReportToFile(templateFilename + ".jrxml",
+		// templateFilename + ".jasper");
+
+		// fill with data
+		String templateResource = getClass().getResource(templateFilename).getFile();
+		System.out.println(templateFilename);
+		System.out.println(templateResource);
+		
+		JasperPrint print = JasperFillManager.fillReport(templateResource,
+				parameters, mapDataSource);
+		
+		// export
+		JRExporter exporter = new JRPdfExporter();
+
+		exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
+		exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, exportToFilename);
+		exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, out);
 
 		exporter.exportReport();
 
--- src/main/java/org/carcv/servlet/GenerateReport.java
+++ src/main/java/org/carcv/servlet/GenerateReport.java
@@ -1,12 +1,8 @@
 package org.carcv.servlet;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.URL;
 
 import javax.ejb.EJB;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -38,49 +34,23 @@ public class GenerateReport extends HttpServlet {
     }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, JRException {
-    	response.setContentType("text/html");
-    	PrintWriter out = response.getWriter();
-    	System.out.println("<html><head><title>CarCV - Download Report</title></head> <body>");
+    	response.setContentType("application/pdf");
+		String fileName = "report"
+				+ System.currentTimeMillis() + ".pdf";
     	
-    	ServletContext context = getServletContext();
+    	response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
     	
+    	//generate
     	long entryId = Long.parseLong(request.getParameter("entry_id"));
     	
     	Entry entry = entryBean.findById(entryId);
     	
-    	//need this to load the image from disk
-    	URL previewImgUrl = context.getResource(entry.getPreview().getURL());
-
-		//temp entry.getPreview().setURL(previewImgUrl.getPath());
+    	System.out.println("DEBUG:\t" + entry.toString()); //debug
 		
-		//generate
-		URL testDir = context.getResource("/");
+    	BasicReportGenerator brg = new BasicReportGenerator();
+    	
+		brg.buildPDFReportStream(entry, "speed_report.jasper", fileName, response.getOutputStream(), "Myjava", "TestReport");
 		
-		System.out.println("testDir " + testDir.getFile() + testDir.getPath());
-
-		File test_results_dir = new File(testDir.getPath() + "/test_results/");
-		if (!test_results_dir.exists() || !test_results_dir.isDirectory()) {
-			test_results_dir.mkdir();
-		}
-		System.out.println("results dir: " + test_results_dir.getPath());
-
-		URL templateUrl = context.getResource(
-				"/reports/speed_report.jasper");
-		
-		File templateFile = new File(templateUrl.getFile());
-		
-		
-		System.out.println("reports template: " + templateUrl.getPath());
-		System.out.println("reports template: " + templateUrl.getFile());
-		System.out.println("reports template: file: " + templateFile.getAbsolutePath());
-
-		String filePath = testDir + "/test_results/report"
-				+ System.currentTimeMillis() + ".pdf";
-		
-		BasicReportGenerator.buildPDFReport(entry, templateUrl.getFile(), filePath, "Myjava", "TestReport");
-		
-		System.out.println("<a href=" + filePath + "> Download here </a>");
-		System.out.println("</body></html>");
 		
 		
 	}
