96114c1aab1b2798fef077cdb0b567ed7b0dc96a
Ondrej Skopek <skopekondrej@gmail.com>
2013-09-23 08:55:47 +0200

    fixed includes in match.cpp

--- carcv-cpp/src/match.hpp
+++ carcv-cpp/src/match.hpp
@@ -7,17 +7,6 @@
  * All rights reserved.
  */
 
-#include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
-#include "opencv2/core/core.hpp"
-#include "opencv2/features2d/features2d.hpp"
-#include "opencv2/nonfree/features2d.hpp"
-#include "opencv2/flann/flann.hpp"
-
-#include <boost/filesystem.hpp>
-
 using namespace std;
 using namespace cv;
 
