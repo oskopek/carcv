0016115a5a4f01fc9a2bb73622534750891c3456
Ondrej Skopek <skopekondrej@gmail.com>
2013-09-24 21:00:30 +0200

    Fixed up integration test handling in maven

--- carcv-webapp/src/test/java/org/carcv/persistence/PersistenceIT.java
+++ carcv-webapp/src/test/java/org/carcv/persistence/PersistenceIT.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.*;
 
 import javax.inject.Inject;
 
-import org.carcv.annotations.IntegrationTest;
 import org.carcv.beans.SpeedBean;
 import org.carcv.model.Speed;
 import org.carcv.model.SpeedUnit;
@@ -20,7 +19,6 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
 import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 /**
@@ -28,8 +26,7 @@ import org.junit.runner.RunWith;
  *
  */
 @RunWith(Arquillian.class)
-@Category(IntegrationTest.class)
-public class PersistenceTest {
+public class PersistenceIT {
     
       
 
