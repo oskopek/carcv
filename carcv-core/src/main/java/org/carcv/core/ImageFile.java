3081251c4900006064071b82ead874a04dc98e9c
Ondrej Skopek <skopekondrej@gmail.com>
2013-10-18 15:38:30 +0200

    Added ImageFileTest
    Signed-off-by: Ondrej Skopek <skopekondrej@gmail.com>

--- carcv-core/src/main/java/org/carcv/core/ImageFile.java
+++ carcv-core/src/main/java/org/carcv/core/ImageFile.java
@@ -3,6 +3,7 @@
  */
 package org.carcv.core;
 
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -17,14 +18,15 @@ import javax.imageio.ImageReadParam;
 import javax.imageio.ImageReader;
 import javax.imageio.stream.ImageInputStream;
 
-import org.apache.commons.lang.builder.CompareToBuilder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 /**
  * Default behavior is to not load an image from path. To load it, use {@link ImageFile#loadImage()}
  * @author oskopek
  * 
  */
-public class ImageFile implements AutoCloseable, Serializable, Comparable<ImageFile>  {
+public class ImageFile implements AutoCloseable, Serializable  {
 
     /**
      * 
@@ -41,7 +43,8 @@ public class ImageFile implements AutoCloseable, Serializable, Comparable<ImageF
 
     public void loadImage() throws IOException {
         InputStream inStream = Files.newInputStream(filepath, StandardOpenOption.READ);
-
+        
+        /*//TODO fix loading of image
         ImageInputStream imageStream = ImageIO.createImageInputStream(inStream);
         ImageReader reader = ImageIO.getImageReaders(imageStream).next();
         ImageReadParam param = reader.getDefaultReadParam();
@@ -52,6 +55,23 @@ public class ImageFile implements AutoCloseable, Serializable, Comparable<ImageF
 
         reader.dispose();
         imageStream.close();
+        */
+        
+        //temp:
+        BufferedImage image = ImageIO.read(inStream);
+
+        if(image == null) {
+            throw new NullPointerException("Failed to load image " + filepath);
+        }
+        
+        BufferedImage outimage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        Graphics2D g = outimage.createGraphics();
+        g.drawImage(image, 0, 0, null);
+        g.dispose();
+
+        this.bufImage = outimage;
+        
         inStream.close();
     }
 
@@ -72,13 +92,54 @@ public class ImageFile implements AutoCloseable, Serializable, Comparable<ImageF
     }
 
     @Override
-    public void close() throws Exception {
-        bufImage.flush();
+    public void close() {
+        if(bufImage!=null) {
+            bufImage.flush();
+        }
     }
 
+    /**
+     * @return the bufImage
+     */
+    public BufferedImage getBufImage() {
+        return bufImage;
+    }
+
+    /**
+     * @param bufImage the bufImage to set
+     */
+    public void setBufImage(BufferedImage bufImage) {
+        this.bufImage = bufImage;
+    }
+
+    /**
+     * @return the filepath
+     */
+    public Path getFilepath() {
+        return filepath;
+    }
+
+    /**
+     * @param filepath the filepath to set
+     */
+    public void setFilepath(Path filepath) {
+        this.filepath = filepath;
+    }
+    
     @Override
-    public int compareTo(ImageFile o) {
-        return new CompareToBuilder().append(bufImage, o.bufImage).append(filepath, o.filepath).toComparison();
+    public boolean equals(Object o) {
+        if(o instanceof ImageFile) {
+            ImageFile f = (ImageFile) o;
+            
+            return new EqualsBuilder().append(bufImage, f.bufImage).append(filepath, f.filepath).isEquals();
+        }        
+        return false;
+        
+    }
+    
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(bufImage).append(filepath).toHashCode();
     }
 
 }
---
+++ carcv-core/src/test/java/org/carcv/core/ImageFileTest.java
@@ -0,0 +1,186 @@
+/**
+ * 
+ */
+package org.carcv.core;
+
+import static org.junit.Assert.*;
+
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author oskopek
+ *
+ */
+public class ImageFileTest {
+    
+    private Path filepath;
+    
+    private ImageFile file;
+
+    /**
+     * @throws URISyntaxException 
+     */
+    @Before
+    public void setUp() throws URISyntaxException {
+        filepath = Paths.get(getClass().getResource("/img/skoda_oct.jpg").toURI());
+        
+        file = new ImageFile(filepath);
+        assertNotNull(file);
+        assertNull(file.getBufImage());
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() {
+        filepath = null;
+        file.close();
+    }
+
+    /**
+     * Test method for {@link org.carcv.core.ImageFile#ImageFile(java.nio.file.Path)}.
+     */
+    @Test
+    public void testImageFile() {
+        
+        Path tmpFile = null;
+        try {
+            tmpFile = Files.createTempFile("imageFile", ".carcv.jpg");
+        } catch (IOException e) {
+            System.err.println("Failed to create temp file");
+            e.printStackTrace();
+        }
+        assertNotNull(tmpFile);
+        
+        ImageFile iFile = new ImageFile(tmpFile);        
+        assertNotNull(iFile);
+        
+        try {
+            iFile.loadImage();
+        } catch (Exception e) {
+            System.err.println(e.getMessage() + " - this is expected");
+            
+        }
+        
+        assertNotNull(iFile);
+        assertNull(iFile.getBufImage());
+    }
+
+    /**
+     * Test method for {@link org.carcv.core.ImageFile#loadImage()}.
+     * @throws IOException 
+     */
+    @Test
+    public void testLoadImage() throws IOException {
+        file.loadImage();
+        assertNotNull(file);
+        assertNotNull(file.getBufImage());
+    }
+
+    /**
+     * Test method for {@link org.carcv.core.ImageFile#loadFragment(java.awt.Rectangle)}.
+     * @throws IOException 
+     */
+    @Test
+    @Ignore(value="Fragment loading not supported yet")
+    public void testLoadFragment() throws IOException {        
+        Rectangle rect = new Rectangle(10, 10, 10, 10);
+        assertNotNull(rect);
+        
+        file.loadFragment(rect);
+        assertNotNull(file);
+        assertNotNull(file.getBufImage());
+    }
+
+    /**
+     * Test method for {@link org.carcv.core.ImageFile#close()}.
+     */
+    @Test
+    public void testCloseNonLoaded() {
+        Path tmpFile = null;
+        try {
+            tmpFile = Files.createTempFile("imageFile", ".carcv.jpg");
+        } catch (IOException e) {
+            System.err.println("Failed to create temp file");
+            e.printStackTrace();
+        }
+        assertNotNull(tmpFile);
+        
+        ImageFile iFile = new ImageFile(tmpFile);        
+        assertNotNull(iFile);
+        
+        iFile.close();
+        assertNotNull(iFile);
+        assertNotNull(iFile.getFilepath());
+        assertNull(iFile.getBufImage());
+    }
+    
+    /**
+     * Test method for {@link org.carcv.core.ImageFile#close()}.
+     */
+    @Test
+    public void testCloseLoadedNonExisting() {
+        Path tmpFile = null;
+        try {
+            tmpFile = Files.createTempFile("imageFile", ".carcv.jpg");
+        } catch (IOException e) {
+            System.err.println("Failed to create temp file");
+            e.printStackTrace();
+        }
+        assertNotNull(tmpFile);
+        
+        ImageFile iFile = new ImageFile(tmpFile);        
+        assertNotNull(iFile);
+        
+        try {
+            iFile.loadImage();
+        } catch (Exception e) {
+            System.err.println(e.getMessage() + " - this is expected");
+            
+        }
+        assertNotNull(iFile);
+        assertNull(iFile.getBufImage());
+        
+        iFile.close();
+        assertNotNull(iFile);
+        assertNotNull(iFile.getFilepath());
+        assertNull(iFile.getBufImage());
+    }
+    
+    /**
+     * Test method for {@link org.carcv.core.ImageFile#close()}.
+     * @throws IOException
+     */
+    @Test
+    public void testCloseLoadedExisting() throws IOException {        
+        ImageFile iFile = new ImageFile(filepath);        
+        assertNotNull(iFile);
+        
+        iFile.loadImage();
+        assertNotNull(iFile);
+        assertNotNull(iFile.getBufImage());
+        
+        //BufferedImage loaded = iFile.getBufImage();
+        
+        iFile.close();
+        assertNotNull(iFile);
+        assertNotNull(iFile.getFilepath());
+        
+        //BufferedImage closed = iFile.getBufImage();
+        
+        //TODO: how to test it is really flushed?
+    }
+
+}
