e4acfba8269e2f29fa1918c0fc88595691769fe2
Ondrej Skopek <skopekondrej@gmail.com>
2013-10-19 23:46:52 +0200

    Remade: Speed
    Signed-off-by: Ondrej Skopek <skopekondrej@gmail.com>

---
+++ carcv-core/src/main/java/org/carcv/core/model/AbstractSpeed.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package org.carcv.core.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.carcv.impl.core.model.Speed;
+
+/**
+ * @author oskopek
+ *
+ */
+@Entity
+//@Table(name = "speed")
+public abstract class AbstractSpeed extends AbstractModel {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    @Column//(name = "speed")
+    @NotNull
+    //@Range(min = 0, message = "Speed is less or equal to 0!")
+    public abstract Double getSpeed();
+    
+
+    @Column//(name = "unit")
+    @NotNull  
+    public abstract SpeedUnit getUnit();
+
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(getSpeed()).append(getUnit()).toHashCode();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Speed)) {
+            return false;
+        }
+        Speed other = (Speed) obj;
+        return new EqualsBuilder()
+                .append(this.getSpeed(), other.getSpeed())
+                .append(this.getUnit(), other.getUnit())
+                .isEquals();
+    }
+
+}
---
+++ carcv-core/src/main/java/org/carcv/impl/core/model/Speed.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package org.carcv.impl.core.model;
+
+import org.carcv.core.model.AbstractSpeed;
+import org.carcv.core.model.SpeedUnit;
+
+/**
+ * @author oskopek
+ * 
+ */
+public class Speed extends AbstractSpeed {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1816208535143255888L;
+
+	/**
+	 * For EJB
+	 */
+	public Speed() {
+		// hibernate constructor stub
+	}
+
+	/**
+	 * @param speed
+	 * @param unit
+	 */
+	public Speed(Double speed, SpeedUnit unit) {
+		this.speed = speed;
+		this.unit = unit;
+	}
+
+	/**
+	 * Default unit = SpeedUnit.MS (meters per second)
+	 * 
+	 * @param speed
+	 */
+	public Speed(Double speed) {
+		this.speed = speed;
+		this.unit = SpeedUnit.MS;
+	}
+
+	private Double speed;
+
+	private SpeedUnit unit;
+
+	/**
+	 * @return the speed
+	 */
+	public Double getSpeed() {
+		return speed;
+	}
+
+	/**
+	 * @param speed
+	 *            the speed to set
+	 */
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	/**
+	 * @return the unit
+	 */
+	public SpeedUnit getUnit() {
+		return unit;
+	}
+
+	/**
+	 * @param unit
+	 *            the unit to set
+	 */
+	public void setUnit(SpeedUnit unit) {
+		this.unit = unit;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	/*
+	@Override
+	public String toString() {
+		return "Speed [speed=" + speed + ", unit=" + unit + "]";
+	}*/
+
+}
