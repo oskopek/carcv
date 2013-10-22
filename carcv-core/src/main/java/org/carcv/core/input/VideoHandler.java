55e99c3d9426cf3be5865c107c0df66359f784ee
Ondrej Skopek <skopekondrej@gmail.com>
2013-10-22 22:12:32 +0200

    Renamed to VideoHandler, added functions to generate a video from frames
    Signed-off-by: Ondrej Skopek <skopekondrej@gmail.com>

---
+++ carcv-core/src/main/java/org/carcv/core/input/VideoHandler.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.carcv.core.input;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+/**
+ * It is encouraged to use a script to split up into frames images externally
+ * @author oskopek
+ */
+public abstract class VideoHandler {
+
+    public abstract boolean disectToFrames(Path video, int frameRate) throws IOException;
+
+    public abstract boolean disectToFrames(Path dir, Path video, int frameRate) throws IOException;
+    
+    public abstract void generateVideo(Path imageDir, Path videoPath, int frameRate) throws IOException;
+    
+    public abstract Path generateVideo(Path imageDir, int frameRate) throws IOException;
+    
+    public abstract OutputStream generateVideoAsStream(Path imageDir, int frameRate) throws IOException;
+
+}
---
+++ carcv-core/src/main/java/org/carcv/impl/core/input/FFMPEG_VideoHandler.java
@@ -0,0 +1,117 @@
+/**
+ * 
+ */
+package org.carcv.impl.core.input;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.carcv.core.input.VideoHandler;
+
+/**
+ * {@linkplain https://trac.ffmpeg.org/wiki/Create%20a%20video%20slideshow%20from%20images}
+ * @author oskopek
+ */
+public class FFMPEG_VideoHandler extends VideoHandler {//TODO: test FFMPEG vid disector
+
+    final public static int defaultFrameRate = 30;
+    
+    final public static String image_suffix = ".png";
+    
+    final public static String video_suffix = ".h264";
+
+    public static boolean disectToFrames(Path video) {
+        FFMPEG_VideoHandler fvd = new FFMPEG_VideoHandler();
+        try {
+            return fvd.disectToFrames(video, defaultFrameRate);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public static OutputStream generateVideo(Path imageDir) {
+        FFMPEG_VideoHandler fvd = new FFMPEG_VideoHandler();
+        try {
+            return fvd.generateVideoAsStream(imageDir, defaultFrameRate);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public boolean disectToFrames(Path video, int frameRate) throws IOException {
+
+        Path dir = Paths.get(video.getParent().toString(), video.getFileName() + ".dir");
+        Files.createDirectory(dir);
+
+        return disectToFrames(dir, video, frameRate);
+    }
+
+    @Override
+    public boolean disectToFrames(Path dir, Path video, int frameRate) throws IOException {
+
+        String filenamePrefix = video.getFileName().toString();
+
+        Path images = Paths.get(dir.toString(), filenamePrefix + "-%09d" + image_suffix);
+
+        String command = "ffmpeg -i " + video.toString() + " -r " + frameRate + " " + images.toString();
+
+        System.out.println("Executing: " + command);
+
+        Process p = Runtime.getRuntime().exec(command);
+        try {
+            p.waitFor();
+        } catch (InterruptedException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public Path generateVideo(Path imageDir, int frameRate) throws IOException {
+        return createVideo(imageDir, frameRate);
+    }
+
+    @Override
+    public OutputStream generateVideoAsStream(Path imageDir, int frameRate) throws IOException {
+        Path tmp = createVideo(imageDir, frameRate);
+        OutputStream ostream = new FileOutputStream(tmp.toFile());
+        return ostream;
+        
+    }
+
+    @Override
+    public void generateVideo(Path imageDir, Path videoPath, int frameRate) throws IOException {
+        Path tmp = createVideo(imageDir, frameRate);
+        Files.move(tmp, videoPath, StandardCopyOption.ATOMIC_MOVE);
+    }
+    
+    private Path createVideo(Path imageDir, int frameRate) throws IOException {
+        Path output = Files.createTempFile("video", video_suffix);
+
+        String command = "ffmpeg -r " + frameRate + " -pattern_type glob -i '" + 
+        imageDir.toAbsolutePath().toString() + File.separator + "*" + image_suffix +
+        "' -c:v libx264 -pix_fmt yuv420p " + output.toAbsolutePath().toString();
+
+        System.out.println("Executing: " + command);
+
+        Process p = Runtime.getRuntime().exec(command);
+        try {
+            p.waitFor();
+        } catch (InterruptedException e) {
+            return null;
+        }
+        
+        return output;
+    }
+
+}
