190d98abb953cef3a12400e6c52b1ad85312e075
Ondrej Skopek <skopekondrej@gmail.com>
2013-10-25 10:41:27 +0200

    Added docs, javadoc comments in CarRecognizer

--- carcv-core/src/main/java/org/carcv/core/recognize/CarRecognizer.java
+++ carcv-core/src/main/java/org/carcv/core/recognize/CarRecognizer.java
@@ -11,6 +11,15 @@ import java.io.IOException;
  */
 public abstract class CarRecognizer {
 
+    /**
+     * This method is supposed to read new <code>Entry</code>-s from one or more data source,
+     * detect individual cars, detect their speed and number plate,
+     * afterwards writing <code>CarData</code> (possibly the <code>List of CarImage-s</code> too)to one or more data sources.
+     * <BR>
+     * Getting the data afterwards is the responsibility of your client externally from the input/output datasources.
+     * 
+     * @throws IOException
+     */
     public abstract void recognize() throws IOException;
 
 }
