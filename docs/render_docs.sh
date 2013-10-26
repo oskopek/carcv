aa40d51e05e1ec2167d36d023d0a468684897592
Ondrej Skopek <skopekondrej@gmail.com>
2013-10-26 19:23:46 +0200

    Fixed docs and removed rendered copies

--- docs/render_docs.sh
+++ docs/render_docs.sh
@@ -10,9 +10,18 @@ cd ${basedir}
 
 rm -rf html/
 mkdir html
+
+if [ -d "${basedir}/core" ]; then
 mkdir html/core
+fi
+
+if [ -d "${basedir}/cpp" ]; then
 mkdir html/cpp
+fi
+
+if [ -d "${basedir}/webapp" ]; then
 mkdir html/webapp
+fi
 
 #Root dir
 
@@ -23,21 +32,40 @@ mv *.html ${basedir}/html/
 
 #Core dir
 
+if [ -d "${basedir}/core" ]; then
+
 cd ${basedir}/core
 
 asciidoctor *.adoc
 mv *.html ${basedir}/html/core/
 
+fi
+
 #CPP dir
+
+if [ -d "${basedir}/cpp" ]; then
 
 cd ${basedir}/cpp
 
 asciidoctor *.adoc
 mv *.html ${basedir}/html/cpp/
 
+fi
+
 #WebApp dir
+
+if [ -d "${basedir}/webapp" ]; then
 
 cd ${basedir}/webapp
 
 asciidoctor *.adoc
 mv *.html ${basedir}/html/webapp/
+
+fi
+
+#docs directory
+if [ -d "${basedir}/docs" ]; then
+
+cd ${basedir}/docs
+./render_docs.sh
+fi
