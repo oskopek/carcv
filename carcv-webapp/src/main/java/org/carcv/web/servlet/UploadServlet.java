9df136653666f3bdd74d9fa35999b264804b6705
Ondrej Skopek <skopekondrej@gmail.com>
2013-10-31 09:30:43 +0100

    Added @WebServlet annotation to UploadServlet

--- carcv-webapp/src/main/java/org/carcv/web/servlet/UploadServlet.java
+++ carcv-webapp/src/main/java/org/carcv/web/servlet/UploadServlet.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,6 +29,7 @@ import org.carcv.web.beans.RecognizerBean;
 /**
  *
  */
+@WebServlet("/servlet/UploadServlet")
 public class UploadServlet extends HttpServlet {
     
     /**
