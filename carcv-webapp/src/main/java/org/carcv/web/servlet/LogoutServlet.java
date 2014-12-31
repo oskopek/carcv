3285fc67f025689fa39b61e0b25693862656b781
Ondrej Skopek <skopekondrej@gmail.com>
2014-12-31 15:28:10 +0100

    Updated copyright notice (year)

--- carcv-webapp/src/main/java/org/carcv/web/servlet/LogoutServlet.java
+++ carcv-webapp/src/main/java/org/carcv/web/servlet/LogoutServlet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2014 CarCV Development Team
+ * Copyright 2012 CarCV Development Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
