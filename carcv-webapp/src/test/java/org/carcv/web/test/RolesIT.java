b987a6c87ff3074c0141d03a74d6869fad63d6cb
Ondrej Skopek <skopekondrej@gmail.com>
2015-03-02 20:12:45 +0100

    Ignored the RolesIT test for now, see issue #42

--- carcv-webapp/src/test/java/org/carcv/web/test/RolesIT.java
+++ carcv-webapp/src/test/java/org/carcv/web/test/RolesIT.java
@@ -20,6 +20,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -30,6 +31,7 @@ import java.io.File;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(Arquillian.class)
+@Ignore
 public class RolesIT {
 
     @Deployment
