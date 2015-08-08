49887db7883bb6c555643c09cd4a4e51ee4a6931
Ondrej Skopek <skopekondrej@gmail.com>
2015-08-08 16:17:08 +0200

    Updated travis script

--- docs/scripts/travis_before.sh
+++ docs/scripts/travis_before.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-export ROOT_DIR=`pwd`
-cd $HOME
-if [ ! -d "$JBOSS_HOME" ]; then
+download_jboss () {
+    root_dir=`pwd`
+    cd $HOME
+    rm -rf "$JBOSS_HOME"
     wget "$WF_LINK"
     tar xfz "wildfly-$WF_VERSION.tar.gz"
+    cd "$root_dir"
+}
+
+if [ ! -d "$JBOSS_HOME" ]; then
+    echo "JBoss dir doesn't exist, downloading wildfly! ( $JBOSS_HOME )"
+    download_jboss
+else
+    echo "JBoss dir exists ( $JBOSS_HOME )"
+    if [ `ls "$JBOSS_HOME" | wc -l` -eq 0 ]; then
+        echo "... but is empty, downloading wildfly!"
+        download_jboss
+    fi
 fi
-cd $ROOT_DIR
-cp $ROOT_DIR/carcv-webapp/src/test/resources/as_configuration/wildfly-standalone.xml $JBOSS_HOME/standalone/configuration/standalone.xml
+cp carcv-webapp/src/test/resources/as_configuration/wildfly-standalone.xml $JBOSS_HOME/standalone/configuration/standalone.xml
