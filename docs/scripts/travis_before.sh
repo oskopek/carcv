a9afe68b167721337c417e3728ae94bf9c41d521
Ondrej Skopek <skopekondrej@gmail.com>
2015-08-08 14:37:07 +0200

    Updated travis config to WildFly

--- docs/scripts/travis_before.sh
+++ docs/scripts/travis_before.sh
@@ -3,8 +3,8 @@
 export ROOT_DIR=`pwd`
 cd $HOME
 if [ ! -d "$JBOSS_HOME" ]; then
-    wget http://download.jboss.org/jbossas/7.1/jboss-as-$JBOSSAS/jboss-as-$JBOSSAS.tar.gz
-    tar xfz jboss-as-$JBOSSAS.tar.gz
+    wget "$WF_LINK"
+    tar xfz "wildfly-$WF_VERSION.tar.gz"
 fi
 cd $ROOT_DIR
-cp $ROOT_DIR/carcv-webapp/src/test/resources/as_configuration/jboss-as-standalone.xml $JBOSS_HOME/standalone/configuration/standalone.xml
+cp $ROOT_DIR/carcv-webapp/src/test/resources/as_configuration/wildfly-standalone.xml $JBOSS_HOME/standalone/configuration/standalone.xml
